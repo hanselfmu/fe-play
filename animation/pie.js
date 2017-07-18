a257b1a7940997dd1d3c12f54b482795b2583736
changhao.han <hanchanghao@wusongtech.com>
2017-07-18 23:47:08 +0800

    add img drawing

--- animation/pie.js
+++ animation/pie.js
@@ -4,17 +4,30 @@ const context = canvas.getContext('2d');
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 
+// pie parameters
 const lineWidth = 4;
 const radius = 70;
 const numberOfPies = 6;
 const angleFactor = 2 * Math.PI / numberOfPies;
+// img parameters
+const imgSources = ['demo1.png', 'demo2.png', 'demo3.png', 'demo4.png', 'demo5.png', 'demo6.png'];
+const imgWidth = 30;
+const imgHeight = 30;
 
+
+// draw pies
 context.beginPath();
 
 for (let i = 0; i < numberOfPies; i++) {
     context.lineTo(centerX, centerY);
 
     context.arc(centerX, centerY, radius, i * angleFactor, (i + 1) * angleFactor, false);
+
+    const img = new Image();
+    img.addEventListener('load', () => {
+        context.drawImage(img, centerX + i * 30, centerY, imgWidth, imgHeight);
+    });
+    img.src = imgSources[i];
 }
 
 context.fillStyle = '#ff5722';
@@ -24,12 +37,12 @@ context.strokeStyle = '#333333';
 context.stroke();
 context.closePath();
 
-// spinner
+// add spinner
 const canvasSpinner = document.getElementById('canvas-spinner');
 canvasSpinner.addEventListener('click', () => {
     canvas.classList.add('spinning');
 
     setTimeout(() => {
         canvas.classList.remove('spinning');
-    }, 3000);
+    }, 8000);
 });
